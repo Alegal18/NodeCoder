2e04359ed0cec588280b8b5f0214e8d6cd939284
Alexis Galvez <cpnalexisgalvez@gmail.com>
2023-09-29 20:05:41 -0300

    finalcambioDeploy

--- src/db/daos/index.js
+++ src/db/daos/index.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
+mongoose.set('strictQuery', false);
 const conection = process.env.db;
 mongoose.connect(conection, error => {
     if (error) {
